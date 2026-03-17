1d5d9a9402aea1f3f747984937218f66739a2db3
Jilanmansuri <jilan.f.mansuri.cg@gmail.com>
2026-03-17 17:08:05 +0530

    connect backend

---
+++ Frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://scamshield-1.onrender.com',
+        changeOrigin: true,
+      },
+    },
+  },
+})
